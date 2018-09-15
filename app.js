78556f8a9c41665fc6f2f61ac1e1af69274ba7c7
Hozefa Mangalorewala <hozefam@gmail.com>
2018-09-15 15:00:18 +0300

    Requirements

---
+++ app.js
@@ -0,0 +1,14 @@
+//requirements
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(bodyParser.json());
+app.use(cors);
+
+//listen
+app.listen(3000, () => {
+  console.log("Server started at 3000 ... ");
+});
